33fba2d1bf899172c3d5b5d2349c44f4e987700a
Jingyue Wu <wujingyue@gmail.com>
2017-11-24 12:48:31 -0800

    Add methods that dedup a string.

--- src/string/MyString.java
+++ src/string/MyString.java
@@ -37,5 +37,34 @@ public class MyString {
 		data.subList(slow_index, data.size()).clear();
 	}
 
+	public void deduplicate() {
+		int slow_index = 0;
+		for (int fast_index = 0; fast_index < data.size(); ++fast_index) {
+			if (slow_index == 0 || data.get(slow_index - 1) != data.get(fast_index)) {
+				data.set(slow_index, data.get(fast_index));
+				++slow_index;
+			}
+		}
+		data.subList(slow_index, data.size()).clear();
+	}
+
+	public void repeatedlyDeduplicate() {
+		int slow_index = 0;
+		int fast_index = 0;
+		while (fast_index < data.size()) {
+			if (slow_index > 0 && data.get(slow_index - 1) == data.get(fast_index)) {
+				while (fast_index < data.size() && data.get(slow_index - 1) == data.get(fast_index)) {
+					++fast_index;
+				}
+				--slow_index;
+			} else {
+				data.set(slow_index, data.get(fast_index));
+				++slow_index;
+				++fast_index;
+			}
+		}
+		data.subList(slow_index, data.size()).clear();
+	}
+
 	private ArrayList<Character> data;
 }
--- src/string/MyStringTest.java
+++ src/string/MyStringTest.java
@@ -22,4 +22,33 @@ class MyStringTest {
 		assertEquals("a b", cloneWithRedundantSpacesRemoved("a  b "));
 	}
 
+	private static String cloneWithDeduplication(String str) {
+		MyString myStr = new MyString(str);
+		myStr.deduplicate();
+		return myStr.toString();
+	}
+
+	@Test
+	void testDeduplicate() {
+		assertEquals("abc", cloneWithDeduplication("abc"));
+		assertEquals("abc", cloneWithDeduplication("aabbc"));
+		assertEquals("aba", cloneWithDeduplication("aabba"));
+		assertEquals("aba", cloneWithDeduplication("aabbaa"));
+		assertEquals("a", cloneWithDeduplication("aa"));
+		assertEquals("a", cloneWithDeduplication("aaaaa"));
+	}
+
+	private static String cloneWithRepeatedDeduplication(String str) {
+		MyString myStr = new MyString(str);
+		myStr.repeatedlyDeduplicate();
+		return myStr.toString();
+	}
+
+	@Test
+	void testRepeatedlyDeduplicate() {
+		assertEquals("abc", cloneWithRepeatedDeduplication("abc"));
+		assertEquals("c", cloneWithRepeatedDeduplication("aabbc"));
+		assertEquals("a", cloneWithRepeatedDeduplication("aabba"));
+		assertEquals("", cloneWithRepeatedDeduplication("aabbaa"));
+	}
 }
